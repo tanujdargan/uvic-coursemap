7a7d1f94337b02c75ad0f3888b6ff1a70a251993
Tanuj Dargan <dargantanuj@gmail.com>
2024-09-22 04:15:21 -0700

    Update PageLayout.tsx
    Potential build fix

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -5,15 +5,22 @@ interface PageLayoutProps {
   children: React.ReactNode
 }
 
+export default function PageLayout() {
+
+const [isMobile, setIsMobile] = useState<boolean>(false);
+const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+}
+
 const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col h-screen bg-gray-900 text-white">
-      <TopBar />
+      <TopBar 
+        isMobile={isMobile}
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}/>
       <div className="flex flex-1 overflow-hidden">
         {children}
       </div>
     </div>
   )
 }
-
-export default PageLayout
